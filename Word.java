82c162b56a68c77bb7f45aae1540aafa6a9acd2f
Apol04 <angel.apo.cisneros@gmail.com>
2025-06-23 14:31:42 -0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,1 +1,32 @@
+//Word.java
 
+public class Word implements Comparable<Word>{
+  private String word;
+
+  public Word (String Word) {
+    this.word = word.toLowerCase();
+  }
+
+  public String getWord(){
+    return word;
+  }
+
+  @Override
+  public int compareTo(Word other) {
+    return this.word.compareTo(other.word);
+  }
+
+  @Override
+  public String toString(){
+    return word;
+  }
+
+  @Override
+  public boolean equals (Object obj) {
+    if (obj instanceof Word) {
+      Word other = (word) obj;
+      return this.word.equals(other.word);
+    }
+    return false;
+  }
+}
